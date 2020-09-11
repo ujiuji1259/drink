833f66b593d663230968ad7ac8b2c4261828e467
Jerry <gaozhiwei1993@gmail.com>
2020-09-11 23:02:48 +0900

    index page optimized

--- static/base.js
+++ static/base.js
@@ -1,7 +1,18 @@
-let base_url = '/drinks'
+var base_url = ''
+if (window.location.href.indexOf('aoi') >= 0) {
+    base_url = '/drinks'
+}
 
 $(function () {
     $('.jump_top').on('click', function () {
         location.href = base_url + "/";
+    });
+
+    $('.jump_logout').on('click', function () {
+        location.href = base_url + "/logout";
+    });
+
+    $('.jump_admin').on('click', function () {
+        location.href = base_url + "/admin";
     })
 })
